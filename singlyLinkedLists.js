2c382150c2e5fe30678e76c66a894d93ca271f08
calvincurry22 <calvincurry22@gmail.com>
2020-09-24 19:42:33 -0500

    Added methods for js single linked list

---
+++ singlyLinkedLists.js
@@ -0,0 +1,64 @@
+//A linked list is a data structure that contains a head, tail, and length property
+//Linked lists consist of nodes, and each node has a value and a pointer to another node or null
+
+//piece of data - val
+//reference to next node - next
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+// var first = new Node("Hi")
+// first.next = new Node("there")
+// first.next.next = new Node("how")
+// first.next.next.next = new Node("are")
+// first.next.next.next.next = new Node("you")
+// console.log(first)
+
+
+class SinglyLinkedList {
+    constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+    push(val) {
+        var newNode = new Node(val)
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    pop() {
+        if (!this.head) {
+            return undefined;
+        }
+        var current = this.head;
+        var newTail = current;
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+}
+
+var list = new SinglyLinkedList()
+list.push("Hello")
+list.push("Goodbye")
+console.log(list)
